8bf857b324eff04b7a497cc3d93bb1a3a291e01b
Mark Berndt <berndtmark@gmail.com>
2025-03-27 17:17:03 +0000

    user name shown

--- StudyCards.UI/studycardsui/src/app/shared/components/shell-page/shell-page.component.ts
+++ StudyCards.UI/studycardsui/src/app/shared/components/shell-page/shell-page.component.ts
@@ -1,18 +1,21 @@
+import { NgIf } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
 import { AuthorizeService } from 'app/shared/services/authorize.service';
+import { RootStore } from 'app/shared/store/root.store';
 
 @Component({
   selector: 'app-shell-page',
-  imports: [RouterOutlet, MatButtonModule, MatIcon],
+  imports: [RouterOutlet, MatButtonModule, MatIcon, NgIf],
   templateUrl: './shell-page.component.html',
   styleUrl: './shell-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ShellPageComponent {
   private authService = inject(AuthorizeService);
+  readonly store = inject(RootStore);
 
   logout() {
     this.authService.logout();
