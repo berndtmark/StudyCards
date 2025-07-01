176527d8d369fe72576e91c8180d135198f84ca7
Mark Berndt <berndtmark@gmail.com>
2025-07-01 18:50:32 +0100

    card additional details

--- StudyCards.UI/studycardsui/src/app/features/card-management/components/card-list/card-list.component.ts
+++ StudyCards.UI/studycardsui/src/app/features/card-management/components/card-list/card-list.component.ts
@@ -4,10 +4,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import { CardResponse } from 'app/@api/models/card-response';
+import {MatSlideToggleChange, MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-card-list',
-  imports: [MatFormFieldModule, MatInputModule, MatTableModule, MatIconModule],
+  imports: [MatFormFieldModule, MatInputModule, MatTableModule, MatIconModule, MatSlideToggleModule, DatePipe],
   templateUrl: './card-list.component.html',
   styleUrl: './card-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -21,11 +23,17 @@ export class CardListComponent {
   });
   updateCard = output<string>();
 
-  displayedColumns: string[] = ['cardfront', 'cardback'];
+  private defaultColumns = ['cardfront', 'cardback'];
+  private extendedColumns =  ['cardfront', 'cardback', 'nextstudy', 'studycount'];
+  displayedColumns: string[] = this.defaultColumns;
   dataSource = new MatTableDataSource<CardResponse>([]);
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  showMoreToggle(change: MatSlideToggleChange) {
+    this.displayedColumns = change.checked ? this.extendedColumns : this.defaultColumns;
   }
 }
