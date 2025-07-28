4174f6c9e4a335389b073eef062ccccf1a8ae760
Mark Berndt <berndtmark@gmail.com>
2025-07-28 22:04:40 +0100

    hide pagination when too small

--- StudyCards.UI/studycardsui/src/app/features/card-management/components/card-list-paginator/card-list-paginator.component.ts
+++ StudyCards.UI/studycardsui/src/app/features/card-management/components/card-list-paginator/card-list-paginator.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import {PageEvent, MatPaginatorModule} from '@angular/material/paginator';
 import { CardStore } from '../../store/card.store';
 
@@ -12,7 +12,9 @@ import { CardStore } from '../../store/card.store';
 export class CardListPaginatorComponent {
   readonly store = inject(CardStore);
 
-  pageSizeOptions = [10, 25, 50];
+  pageSizeOptions = [10, 25, 100];
+
+  totalPages = computed(() => Math.ceil(this.store.pagination.totalCount() / this.store.pagination.pageSize()));
 
   handlePageEvent(e: PageEvent) {
     this.store.paginate(e.pageIndex + 1, e.pageSize)
