57dafb60747ae5d1ed37fd052f946e4420d07f17
Mark Berndt <berndtmark@gmail.com>
2025-11-12 09:12:52 +0000

    fixed issues

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # StudyCards
-
 ![workflow](https://github.com/berndtmark/studycards/actions/workflows/ci.yml/badge.svg)
 ![workflow](https://github.com/berndtmark/studycards/actions/workflows/azure-container-webapp.yml/badge.svg)
+
+# Auth Signin
+Get cookies needed to run the app from https://localhost:5102/api/auth/login
--- StudyCards.Api/Program.cs
+++ StudyCards.Api/Program.cs
@@ -9,26 +9,43 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
+// LOGGING
 builder.Host.ConfigureLogging();
+
+// EXCEPTION HANDLING
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddProblemDetails();
 
+// SECRETS (FOR CONFIGURATION)
 builder.AddSecretsConfiguration();
+
+// CONTROLLERS
 builder.Services.AddControllers();
+
+// OPENAPI
 builder.Services.AddOpenApi();
+
+// CACHE
 builder.Services.AddMemoryCache();
 
+// SIGNAL R
 builder.Services.AddSignalR();
 builder.Services.AddSingleton<IUserIdProvider, EmailUserIdProvider>();
 
+// SECURITY
 builder.Services.AddSecurityConfiguration(builder.Configuration);
+
+// MAPPERS
 builder.Services.AddMappingConfiguration();
+
+// OPTIONS
 builder.Services.AddOptionsConfiguration(builder.Configuration);
 
-// Configure Application Layers
+// BOOTSTRAP APPLICATION LAYERS
 builder.Services.ConfigureApplicationServices();
 builder.Services.ConfigureInfrastructureDatabaseServices(builder.Configuration);
 
+// BUILD
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
