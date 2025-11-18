2f3e5cdd40aa475c715d2361b9606d0f010da8e2
Mark Berndt <berndtmark@gmail.com>
2025-11-18 17:25:28 +0000

    init (#81)

--- Tests/StudyCards.Application.Tests/UseCases/GetCardsToStudyQueryTests.cs
+++ Tests/StudyCards.Application.Tests/UseCases/GetCardsToStudyQueryTests.cs
@@ -1,11 +1,9 @@
 ﻿using Moq;
 using StudyCards.Application.Exceptions;
-using StudyCards.Application.Interfaces;
 using StudyCards.Application.Interfaces.Repositories;
 using StudyCards.Application.UseCases.CardStudy.Queries;
 using StudyCards.Domain.Entities;
 using StudyCards.Domain.Enums;
-using StudyCards.Domain.Interfaces;
 
 namespace StudyCardy.Application.Tests.UseCases;
 
@@ -14,8 +12,6 @@ public class GetCardsToStudyQueryTests
 {
     private Mock<IDeckRepository> _deckRepositoryMock = default!;
     private Mock<ICardRepository> _cardRepositoryMock = default!;
-    private Mock<ICardsToStudyStrategyContext> _cardStrategyContextMock = default!;
-    private Mock<ICardSelectionStudyFactory> _cardSelectionStudyFactoryMock = default!;
     private GetCardsToStudyQueryHandler _useCase = default!;
 
     [TestInitialize]
@@ -23,14 +19,10 @@ public class GetCardsToStudyQueryTests
     {
         _deckRepositoryMock = new Mock<IDeckRepository>();
         _cardRepositoryMock = new Mock<ICardRepository>();
-        _cardStrategyContextMock = new Mock<ICardsToStudyStrategyContext>();
-        _cardSelectionStudyFactoryMock = new Mock<ICardSelectionStudyFactory>();
 
         _useCase = new GetCardsToStudyQueryHandler(
             _deckRepositoryMock.Object,
-            _cardRepositoryMock.Object,
-            _cardStrategyContextMock.Object,
-            _cardSelectionStudyFactoryMock.Object
+            _cardRepositoryMock.Object
         );
     }
 
@@ -41,21 +33,16 @@ public class GetCardsToStudyQueryTests
         var deckId = Guid.NewGuid();
         var deck = new Deck { Id = deckId, DeckSettings = new DeckSettings { ReviewsPerDay = 10 } };
         var cards = new List<Card> { new(), new() };
-        var strategy = Mock.Of<ICardsToStudyStrategy>();
 
         _deckRepositoryMock.Setup(x => x.Get(deckId, CancellationToken.None))
             .ReturnsAsync(deck);
-        _cardRepositoryMock.Setup(x => x.GetByDeck(deckId, CancellationToken.None))
+        _cardRepositoryMock.Setup(x => x.GetCardsToStudy(deckId, It.IsAny<int>(), It.IsAny<int>(), It.IsAny<bool>(), CancellationToken.None))
             .ReturnsAsync(cards);
-        _cardSelectionStudyFactoryMock.Setup(x => x.Create(It.IsAny<CardStudyMethodology>()))
-            .Returns(strategy);
-        _cardStrategyContextMock.Setup(x => x.GetStudyCards(deck.DeckSettings.ReviewsPerDay, deck.DeckSettings.NewCardsPerDay))
-            .Returns(cards);
 
         var request = new GetCardsToStudyQuery
         {
             DeckId = deckId,
-            StudyMethodology = CardStudyMethodology.Random
+            StudyMethodology = CardStudyMethodology.Anki
         };
 
         // Act
@@ -64,8 +51,6 @@ public class GetCardsToStudyQueryTests
         // Assert
         Assert.IsNotNull(result);
         Assert.AreEqual(cards.Count, result.Count());
-        _cardStrategyContextMock.Verify(x => x.SetStrategy(strategy), Times.Once);
-        _cardStrategyContextMock.Verify(x => x.AddCards(cards), Times.Once);
     }
 
     [TestMethod]
