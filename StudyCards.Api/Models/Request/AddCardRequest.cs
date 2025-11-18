61c1e1b252f67cf6cbb6cefb394151a9c5c85663
Mark Berndt <berndtmark@gmail.com>
2025-11-18 15:54:37 +0000

    required controller model values

--- StudyCards.Api/Models/Request/AddCardRequest.cs
+++ StudyCards.Api/Models/Request/AddCardRequest.cs
@@ -1,8 +1,13 @@
-﻿namespace StudyCards.Api.Models.Request;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudyCards.Api.Models.Request;
 
 public class AddCardRequest
 {
+    [Required]
     public Guid DeckId { get; set; }
+    [Required]
     public string CardFront { get; set; } = string.Empty;
+    [Required]
     public string CardBack { get; set; } = string.Empty;
 }
--- StudyCards.Api/Models/Request/UpdateDeckRequest.cs
+++ StudyCards.Api/Models/Request/UpdateDeckRequest.cs
@@ -1,10 +1,18 @@
-﻿namespace StudyCards.Api.Models.Request;
+﻿using StudyCards.Api.Models.Validators;
+using System.ComponentModel.DataAnnotations;
 
-public class UpdateDeckRequest
+namespace StudyCards.Api.Models.Request;
+
+[MaxNewCardsValidator]
+public class UpdateDeckRequest : IAddUpdateDeckRequest
 {
+    [Required]
     public Guid DeckId { get; set; }
+    [Required]
     public string DeckName { get; set; } = string.Empty;
     public string? Description { get; set; } = string.Empty;
+    [Required]
     public int ReviewsPerDay { get; set; }
+    [Required]
     public int NewCardsPerDay { get; set; }
 }
