6a4bce52a3df3190a4f9eb7f7011fb01b401741f
Mark Berndt <berndtmark@gmail.com>
2026-03-09 21:04:31 +0000

    Fix caching redirect (#109)

---
+++ StudyCards.Api/Configuration/StaticFileConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace StudyCards.Api.Configuration;
+
+public static class StaticFileConfiguration
+{
+    public static StaticFileOptions StaticFileOptions => new()
+    {
+        OnPrepareResponse = ctx =>
+        {
+            if (ctx.File.Name.Equals("index.html", StringComparison.OrdinalIgnoreCase))
+            {
+                ctx.Context.Response.Headers.Append("Cache-Control", "no-cache, no-store");
+                ctx.Context.Response.Headers.Append("Pragma", "no-cache");
+                ctx.Context.Response.Headers.Append("Expires", "-1");
+            }
+        }
+    };
+}
